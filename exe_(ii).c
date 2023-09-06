81588d23e560154389d408a486cdbeaee91e42b4
SFernando Solutions <143948014+SFernandoSolutions@users.noreply.github.com>
2023-09-06 18:04:11 +0530

    Create exe_(ii).c

---
+++ exe_(ii).c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void){
+	int x;
+	
+	x=10;
+	
+	do{
+		printf("%d\n",x);
+		x--;
+	}while (x>1);
+	
+	return 0;
+}
+
+
+//x=10;
+//Out put
+//10
+//9
+//8
+//7
+//6
+//5
+//4
+//3
+//2
